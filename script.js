d9d2002a3f58b84bb265e07ad51a3bae275c5d9b
dansch1982 <dansch1982@gmail.com>
2022-01-28 11:31:01 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const   speed = 2000,
+        appearance = 1000,
+        holes = document.querySelectorAll('main article');
+
+for (const hole of holes) {
+    hole.addEventListener('click', function() {
+        if (this.classList.contains('mole')) {
+            this.classList.remove('mole')
+            this.classList.toggle('hit')
+            setTimeout(() => {
+                this.classList.toggle('hit')
+            }, 800);
+            const element = document.querySelector('.moleswhacked b')
+            element.textContent++
+        }
+    });
+};
+
+function loop() {
+    const element = document.querySelector('.timeleft b')
+    const secondsLeft = element.textContent.substring(0, element.textContent.length-1)
+    if (secondsLeft <= 0) {
+        const element = document.querySelector('.mole')
+        if (element) {
+            element.classList.remove('mole')
+        }
+        return console.log("game stopped")
+    }
+    const index = Math.floor(Math.random() * holes.length)
+    const whacked = document.querySelector('.moleswhacked b').textContent
+    setTimeout(() => {
+        if (secondsLeft % (speed/1000) === 0) {
+            holes[index].classList.add("mole")
+            setTimeout(() => {
+                holes[index].classList.remove("mole")
+            }, appearance - whacked * 25);
+        }
+        element.textContent = secondsLeft - 1 + "s"
+        loop();
+    }, 1000);
+}
+loop();
