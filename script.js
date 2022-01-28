e8cf08c5e96302c3e69df0133196e7abe2f643ed
dansch1982 <dansch1982@gmail.com>
2022-01-28 11:42:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ for (const hole of holes) {
 function loop() {
     const element = document.querySelector('.timeleft b')
     const secondsLeft = element.textContent.substring(0, element.textContent.length-1)
+    const whacked = document.querySelector('.moleswhacked b').textContent
     if (secondsLeft <= 0) {
         const element = document.querySelector('.mole')
         if (element) {
             element.classList.remove('mole')
         }
-        return console.log("game stopped")
+        return alert(`Slut på tid! Du lyckades slå ${whacked} mulvadar.`)
     }
     const index = Math.floor(Math.random() * holes.length)
-    const whacked = document.querySelector('.moleswhacked b').textContent
     setTimeout(() => {
         if (secondsLeft % (speed/1000) === 0) {
             holes[index].classList.add("mole")
