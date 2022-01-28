8b8126180295b2744277d368e026092cccc5bcc2
dansch1982 <dansch1982@gmail.com>
2022-01-28 11:46:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const   speed = 2000,
-        appearance = 1000,
-        holes = document.querySelectorAll('main article');
+const speed = 2000,
+    appearance = 1000,
+    holes = document.querySelectorAll('main article');
 
 for (const hole of holes) {
-    hole.addEventListener('click', function() {
+    hole.addEventListener('click', function () {
         if (this.classList.contains('mole')) {
             this.classList.remove('mole')
             this.classList.toggle('hit')
@@ -18,7 +18,7 @@ for (const hole of holes) {
 
 function loop() {
     const element = document.querySelector('.timeleft b')
-    const secondsLeft = element.textContent.substring(0, element.textContent.length-1)
+    const secondsLeft = element.textContent.substring(0, element.textContent.length - 1)
     const whacked = document.querySelector('.moleswhacked b').textContent
     if (secondsLeft <= 0) {
         const element = document.querySelector('.mole')
@@ -29,7 +29,7 @@ function loop() {
     }
     const index = Math.floor(Math.random() * holes.length)
     setTimeout(() => {
-        if (secondsLeft % (speed/1000) === 0) {
+        if (secondsLeft % (speed / 1000) === 0) {
             holes[index].classList.add("mole")
             setTimeout(() => {
                 holes[index].classList.remove("mole")
@@ -39,4 +39,6 @@ function loop() {
         loop();
     }, 1000);
 }
-loop();
+window.addEventListener('DOMContentLoaded', () => {
+    loop();
+});
